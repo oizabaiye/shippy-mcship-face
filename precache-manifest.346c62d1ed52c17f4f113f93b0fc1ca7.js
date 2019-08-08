3fba92487bb790e779a092bb0ed1d407e369c092
Ahuoiza <oizabaiye@gmail.com>
2019-08-08 02:23:24 -0400

    Updates

---
+++ precache-manifest.346c62d1ed52c17f4f113f93b0fc1ca7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e62502538ce6f9001a0cda78335c955",
+    "url": "/shippymcshipface/index.html"
+  },
+  {
+    "revision": "d4e1d7ca44b487a35131",
+    "url": "/shippymcshipface/static/css/main.60bcccbd.chunk.css"
+  },
+  {
+    "revision": "02eede51ef082dee9be2",
+    "url": "/shippymcshipface/static/js/2.fef3e536.chunk.js"
+  },
+  {
+    "revision": "d4e1d7ca44b487a35131",
+    "url": "/shippymcshipface/static/js/main.b5e5a277.chunk.js"
+  },
+  {
+    "revision": "e644c36b48ba2c588750",
+    "url": "/shippymcshipface/static/js/runtime~main.7c2d3df0.js"
+  },
+  {
+    "revision": "fa13e3e709b5a4dd34a4cc4193c32147",
+    "url": "/shippymcshipface/static/media/starship.fa13e3e7.jpeg"
+  }
+]);
